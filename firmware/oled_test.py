22aea18b06619037d790fabd5e0bf452ad44f450
Xerette <shouryapatel05@gmail.com>
2026-07-27 17:44:46 -0400

    Update oled_test.py

--- firmware/oled_test.py
+++ firmware/oled_test.py
@@ -4,7 +4,7 @@ from ssd1306 import SSD1306_I2C
 i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=400000)
 print(i2c.scan())  # sanity check — should now show 3 addresses
 
-oled = SSD1306_I2C(128, 64, i2c)
+oled = SSD1306_I2C(128, 64, i2c, addr=0x3d)
 
 oled.fill(0)
 oled.text("Sensor Payload", 0, 0)
