31ab985f8c7b06d3a2ed31b3ea9354bfc3c96441
Xerette <shouryapatel05@gmail.com>
2026-07-27 16:19:34 -0400

    Create bmi270_read_test.py
    Checkpoint 3B: BMI270 acceleration + gyro readings working

---
+++ firmware/bmi270_read_test.py
@@ -0,0 +1,13 @@
+from machine import Pin, I2C
+from micropython_bmi270 import bmi270
+import time
+
+i2c = I2C(0, sda=Pin(0), scl=Pin(1), freq=100000)
+bmi = bmi270.BMI270(i2c)
+
+while True:
+    accx, accy, accz = bmi.acceleration
+    gyrox, gyroy, gyroz = bmi.gyro
+    print("accel: x={:.2f} y={:.2f} z={:.2f} m/s2 | gyro: x={:.2f} y={:.2f} z={:.2f} deg/s".format(
+        accx, accy, accz, gyrox, gyroy, gyroz))
+    time.sleep(0.5)
